edebdbe69fd3a0242c9f011bd178b09f3ce6db42
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-09 09:46:57 +0300

    created and modified Navbar.js at the top of the page

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React,{useState} from 'react'
+import {Hamburger, Logo, Menu, MenuLink, Nav} from "./Navbarstyle"
+
+const Navbar = () => {
+    const[isOpen,setIsOpen] = useState(false)
+    return (
+        <Nav>
+            <Logo>
+            <i>{"<HAKAN>"}</i> <span>recipe</span>
+            </Logo>
+        
+        <Hamburger onClick={() => setIsOpen(!isOpen)}>
+            {/* 3 span provide us three horizontal line inside Hamburger Menu */}
+            {/* with onClick we change the height of menu from 0 to 300 by this way it become visible */}
+           <span/>
+           <span/>
+           <span/>
+        </Hamburger>
+        <Menu isOpen = {isOpen}>
+            {/* we send isOpen as props to Menu inside navbarstyle so we change height 
+            according to value of isOpen  */}
+            <MenuLink to="/about">About</MenuLink>
+            <MenuLink to={{pathname:"https://github.com/hakankaraahmet"}} target="_blank">GitHub</MenuLink>
+            <MenuLink to="/login">Logout</MenuLink>
+        </Menu>
+        </Nav>
+    )
+}
+
+export default Navbar
