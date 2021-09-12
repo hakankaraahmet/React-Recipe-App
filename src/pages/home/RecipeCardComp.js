5e9f9a41e45f268b94f7273a6e0502966ab19ffc
hakankaraahmet <karaahmethkn@gmail.com>
2021-09-12 17:39:10 +0300

    modified RecipeCardComp.js with useHistory

--- src/pages/home/RecipeCardComp.js
+++ src/pages/home/RecipeCardComp.js
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 import { Button } from '../../components/header/HeaderStyle'
 import { RecipeCard, RecipeHeader, RecipeImage } from './HomeStyle'
 import defaultImg from "../../assets/default-image.jpg"
+
+
 const RecipeCardComp = ({recipe}) => {
+    const history = useHistory();
+    const moreClick = () => {
+        //here we are going to use useHistory to direct to details page
+        history.push({
+            pathname: "/details",
+            recipe : recipe // here we send recipe as props to details page for later use
+        })
+    }
+    
     return (
         <RecipeCard>
             <RecipeHeader>
                 {recipe.label}
             </RecipeHeader>
             <RecipeImage src={recipe?.image ?? defaultImg} alt = "recipe image"/>
-            <Button>
+            <Button onClick = {moreClick}>
                 View More
             </Button>
         </RecipeCard>
